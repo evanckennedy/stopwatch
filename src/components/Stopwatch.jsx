bd7c4d6a373a80787fdab7c5f57d5e437b3b8e86
Evan Kennedy <evanckennedy@protonmail.com>
2024-05-13 15:15:22 -0500

    initial commit

---
+++ src/components/Stopwatch.jsx
@@ -0,0 +1,12 @@
+import { useState, useEffect, useRef } from "react"
+
+function Stopwatch() {
+  const [isRunning, setIsRunning] = useState(false);
+  const [elapsedTime, setElapsedTime] = useState
+
+  return (
+    <div>Stopwatch</div>
+  )
+}
+
+export default Stopwatch
