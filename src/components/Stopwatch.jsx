eaeac615f3a6e68ed14a82a4a79307b601b69890
Evan Kennedy <evanckennedy@protonmail.com>
2024-05-13 20:46:19 -0500

    create basic stopwatch functionality

--- src/components/Stopwatch.jsx
+++ src/components/Stopwatch.jsx
@@ -7,27 +7,44 @@ function Stopwatch() {
   const startTimeRef = useRef(0);
 
   useEffect(() => {
+    if (isRunning) {
+      intervalIdRef.current = setInterval(() => {
+        setElapsedTime(Date.now() - startTimeRef.current)
+      }, 10);
+    }
 
+    return () => {
+      clearInterval(intervalIdRef.current);
+    }
   }, [isRunning])
 
-  function start() {
 
+  function start() {
+    setIsRunning(true);
+    startTimeRef.current = Date.now() - elapsedTime;
   }
 
   function stop() {
-
+    setIsRunning(false)
   }
 
   function reset() {
-
+    setElapsedTime(0);
+    setIsRunning(false)
   }
 
   function formatTime() {
-    return `00:00:00`;
+    let hours = Math.floor(elapsedTime / (1000 * 60 * 60)).toString().padStart(2, '0');
+    let minutes = Math.floor(elapsedTime / (1000 * 60) % 60).toString().padStart(2, '0');
+    let seconds = Math.floor(elapsedTime / (1000) % 60).toString().padStart(2, '0');
+    let milliseconds = Math.floor(elapsedTime % 1000 / 10).toString().padStart(2, '0');
+
+    return `${minutes}:${seconds}:${milliseconds}`;
   }
+
   return (
     <main className="center">
-      <div className="stopwatch">
+      <div className="stopwatch flex flex-column gap-20">
         <div className="display">{formatTime()}</div>
         <div className="controls flex justify-between">
           <button className="start-button" onClick={start}>Start</button>
@@ -35,8 +52,7 @@ function Stopwatch() {
           <button className="reset-button" onClick={reset}>Reset</button>
         </div>
       </div>
-    </main>
-    
+    </main> 
   )
 }
 
