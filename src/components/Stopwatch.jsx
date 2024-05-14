1ff8ccf68812f46c695f9aee80ac28c861623163
Evan Kennedy <evanckennedy@protonmail.com>
2024-05-13 20:22:50 -0500

    basic styling complete

--- src/components/Stopwatch.jsx
+++ src/components/Stopwatch.jsx
@@ -2,10 +2,41 @@ import { useState, useEffect, useRef } from "react"
 
 function Stopwatch() {
   const [isRunning, setIsRunning] = useState(false);
-  const [elapsedTime, setElapsedTime] = useState
+  const [elapsedTime, setElapsedTime] = useState(0);
+  const intervalIdRef = useRef(null);
+  const startTimeRef = useRef(0);
 
+  useEffect(() => {
+
+  }, [isRunning])
+
+  function start() {
+
+  }
+
+  function stop() {
+
+  }
+
+  function reset() {
+
+  }
+
+  function formatTime() {
+    return `00:00:00`;
+  }
   return (
-    <div>Stopwatch</div>
+    <main className="center">
+      <div className="stopwatch">
+        <div className="display">{formatTime()}</div>
+        <div className="controls flex justify-between">
+          <button className="start-button" onClick={start}>Start</button>
+          <button className="stop-button" onClick={stop}>Stop</button>
+          <button className="reset-button" onClick={reset}>Reset</button>
+        </div>
+      </div>
+    </main>
+    
   )
 }
 
